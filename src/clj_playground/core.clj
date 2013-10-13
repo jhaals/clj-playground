728124d578f67c0b41dc18207f679f5c0d05bef9
Vincent Ambo <vincent@spotify.com>
2013-10-13 13:07:14 +0200

    Fixed your indentation bra

--- src/clj_playground/core.clj
+++ src/clj_playground/core.clj
@@ -1,13 +1,13 @@
 (ns clj-playground.core
-    (:require  [clj-http.client :as client])
-    (:require  [cheshire.core :refer :all]))
+  (:require  [clj-http.client :as client])
+  (:require  [cheshire.core :refer :all]))
 
 (defn pug
-    "Return random pug"
-    []
-    (:pug  (parse-string
-                       (:body  (client/get "https://pugme.herokuapp.com/random"))
-                       true)))
+  "Return random pug"
+  []
+  (:pug  (parse-string
+          (:body  (client/get "https://pugme.herokuapp.com/random"))
+          true)))
 
 (defn main  []
-    (println  (pug)))
+  (println  (pug)))
